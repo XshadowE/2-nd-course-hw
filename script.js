b68699b5dd297f95e0fdc0950fc315e3257de245
XshadowE <nimro-dell@mail.ru>
2023-07-30 14:37:14 +0700

    added variable

---
+++ script.js
@@ -0,0 +1,67 @@
+let a;
+a = 10;
+alert (a);
+
+a = 20;
+alert (a);
+
+let year = 2007;
+// year = prompt('Год выпуска первого iPhone');
+alert (year);
+
+let userName = "Брендан Эйх";
+// Имя создателя языка JavaScript');
+alert (userName);
+
+let x = 10;
+let y = 2;
+alert (x + y);
+alert (x - y);
+alert (x / y);
+alert (x * y);
+
+let b = 2;
+result = b ** 5;
+alert(b ** 5);
+
+a = 9;
+b = 2;
+alert (a / b);
+
+let num = 1;
+console.log (num++ + 5); //6
+console.log (num++ - 3); //-1
+console.log (--num * 7); //14
+console.log (num++ / 3); //0.6666
+console.log (--num + 1); //3
+console.log (num++ + -1); //1
+alert(num);
+
+let age;
+age = prompt('Сколько вам лет?');
+alert (age);
+
+const user = {
+    userName: 'Вася',
+    age: 28,
+    isAdmin: true,
+    ["city of residence"]:'Новосибирск',
+    delete: ["city of residence"],
+} 
+console.log(user);
+
+user.age = 33;
+console.log(user.age);
+
+let info = prompt('Какую информацию хотите узнать о пользователе?');
+alert( user [info] );
+
+let newNameUser = prompt('Как Вас зовут?');
+alert( `Привет, ${newNameUser} !`);
+ 
+
+
+
+
+
+
