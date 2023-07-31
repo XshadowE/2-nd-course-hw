311f4b6bf4d9d2c142f4185e37505379d57633ec
XshadowE <nimro-dell@mail.ru>
2023-07-31 10:06:15 +0700

    added fix

---
+++ dist/script.dev.js
@@ -0,0 +1,69 @@
+"use strict";
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+// задание 1
+var a;
+a = 10;
+alert(a);
+a = 20;
+alert(a); // задание 2
+
+var year = 2007; // year = prompt('Год выпуска первого iPhone');
+
+alert(year); // задание 3
+
+var userName = "Брендан Эйх"; // Имя создателя языка JavaScript');
+
+alert(userName); // задание 4
+
+var x = 10;
+var y = 2;
+alert(x + y);
+alert(x - y);
+alert(x / y);
+alert(x * y); // задание 5
+
+var b = 2;
+result = Math.pow(b, 5);
+alert(Math.pow(b, 5)); // задание 6
+
+a = 9;
+b = 2;
+alert(a % b); // задание 7
+
+var num = 1;
+console.log(num++ + 5); //6
+
+console.log(num++ - 3); //-1
+
+console.log(--num * 7); //14
+
+console.log(num++ / 3); //0.6666
+
+console.log(--num + 1); //3
+
+console.log(num++ + -1); //1
+
+alert(num); // задание 8
+
+var age;
+age = prompt('Сколько вам лет?');
+alert(age); // задание 9
+
+var user = _defineProperty({
+  userName: 'Вася',
+  age: 28,
+  isAdmin: true
+}, "city of residence", 'Новосибирск');
+
+console.log(user);
+delete user["city of residence"];
+var key = 'age';
+user[key] = 33;
+console.log(user);
+var info = prompt('Какую информацию хотите узнать о пользователе?');
+alert(user[info]); // задание 10
+
+var newNameUser = prompt('Как Вас зовут?');
+alert("\u041F\u0440\u0438\u0432\u0435\u0442, ".concat(newNameUser, " !"));
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// задание 1
 let a;
 a = 10;
 alert (a);
@@ -5,14 +6,17 @@ alert (a);
 a = 20;
 alert (a);
 
+// задание 2
 let year = 2007;
 // year = prompt('Год выпуска первого iPhone');
 alert (year);
 
+// задание 3
 let userName = "Брендан Эйх";
 // Имя создателя языка JavaScript');
 alert (userName);
 
+// задание 4
 let x = 10;
 let y = 2;
 alert (x + y);
@@ -20,14 +24,17 @@ alert (x - y);
 alert (x / y);
 alert (x * y);
 
+// задание 5
 let b = 2;
 result = b ** 5;
 alert(b ** 5);
 
+// задание 6
 a = 9;
 b = 2;
-alert (a / b);
+alert (a % b);
 
+// задание 7
 let num = 1;
 console.log (num++ + 5); //6
 console.log (num++ - 3); //-1
@@ -37,25 +44,31 @@ console.log (--num + 1); //3
 console.log (num++ + -1); //1
 alert(num);
 
+// задание 8
 let age;
 age = prompt('Сколько вам лет?');
 alert (age);
 
+// задание 9
 const user = {
     userName: 'Вася',
     age: 28,
     isAdmin: true,
-    ["city of residence"]:'Новосибирск',
-    delete: ["city of residence"],
+    ["city of residence"]:'Новосибирск'
 } 
 console.log(user);
 
-user.age = 33;
-console.log(user.age);
+delete user ["city of residence"];
+
+
+const key = 'age';
+user [key] = 33;
+console.log(user);
 
 let info = prompt('Какую информацию хотите узнать о пользователе?');
-alert( user [info] );
+alert(user[info]);
 
+// задание 10
 let newNameUser = prompt('Как Вас зовут?');
 alert( `Привет, ${newNameUser} !`);
  
